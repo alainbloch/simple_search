6e91ea80aa8881d7d2cb80e3ecccedc58681fb6f
alain <alainbloch@gmail.com>
2009-12-14 14:51:09 -0800

    Added tests for simple_search.

---
+++ test/schema.rb
@@ -0,0 +1,9 @@
+ActiveRecord::Schema.define(:version => 0) do
+  
+  create_table :mocks, :force => true do |t|
+    t.column :mock_string, :string
+    t.column :mock_text, :text
+    t.column :position, :integer
+  end
+
+end
